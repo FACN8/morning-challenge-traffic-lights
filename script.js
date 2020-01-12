b94e5f25fadcc723506a28693aabe964ce0d49ed
Jamie Coe <jamiecoe66@gmail.com>
2020-01-12 11:33:46 +0200

    update solution to match README instructions

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 // DON'T CHANGE ANYTHING BETWEEN HERE ---
-var time = 1000;
+var time = 1000
 
-var red = blinkLight(get('light-top'), 'red');
-var yellow = blinkLight(get('light-middle'), 'yellow');
-var green = blinkLight(get('light-bottom'), 'green');
+var red = blinkLight(get('light-top'), 'red')
+var yellow = blinkLight(get('light-middle'), 'yellow')
+var green = blinkLight(get('light-bottom'), 'green')
 
-var red2 = blinkLight(get('light-top2'), 'red');
-var yellow2 = blinkLight(get('light-middle2'), 'yellow');
-var green2 = blinkLight(get('light-bottom2'), 'green');
+var red2 = blinkLight(get('light-top2'), 'red')
+var yellow2 = blinkLight(get('light-middle2'), 'yellow')
+var green2 = blinkLight(get('light-bottom2'), 'green')
 
 function blinkLight(element, colour) {
   var blink = function(callback) {
-    element.classList.add(colour);
+    element.classList.add(colour)
     setTimeout(function() {
-      element.classList.remove(colour);
+      element.classList.remove(colour)
       if (callback) {
-        callback();
+        callback()
       }
-    }, time);
-  };
+    }, time)
+  }
 
-  return blink;
+  return blink
 }
 
 function get(id) {
-  return document.getElementById(id);
+  return document.getElementById(id)
 }
 
 // --- AND HERE.
@@ -32,44 +32,42 @@ function get(id) {
 
 function light(callback) {
   green(function() {
-    green(function() {
-      yellow(function() {
+    yellow(function() {
+      red(function() {
         red(function() {
           red(function() {
-            red(function() {
-              red(function() {
-                red();
-                yellow(function() {
-                  callback(callback);
-                });
-              });
-            });
-          });
-        });
-      });
-    });
-  });
+            red()
+            yellow(function() {
+              green(function() {
+                callback(callback)
+              })
+            })
+          })
+        })
+      })
+    })
+  })
 }
-light(light);
+light(light)
 
 function light2(callback) {
-  red2(function() {
-    red2(function() {
-      red2(function() {
-        yellow2();
-        red2(function() {
+  red2(function() {    
+    red2()
+    yellow2(function() {      
+      green2(function() {
+        green2(function() {
           green2(function() {
-            green2(function() {
-              yellow2(function() {
-                red2(function() {
-                  callback(callback);
-                });
-              });
-            });
-          });
-        });
-      });
-    });
-  });
+            green2()
+            red2()
+            yellow2(function() {
+              red2(function() {
+                callback(callback)
+              })
+            })
+          })
+        })
+      })
+    })
+  })
 }
-light2(light2);
+light2(light2)
